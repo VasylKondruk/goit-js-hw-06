4bb37981dde91a4ac334a804be337aaf7f316a50
Vasyl Kondruk <vkondruk16@gmail.com>
2022-07-11 23:53:18 +0300

    udpate task-6

--- js/task-06.js
+++ js/task-06.js
@@ -5,9 +5,11 @@ inputRef.addEventListener('blur', blur);
 function blur(event) {
   if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
     inputRef.classList.remove('invalid');
+
     return inputRef.classList.add('valid');
   } else {
     inputRef.classList.remove('valid');
+
     return inputRef.classList.add('invalid');
   }
 }
