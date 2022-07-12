a90421bdc584800879884e158a436aea81612ee9
Vasyl Kondruk <vkondruk16@gmail.com>
2022-07-12 13:30:21 +0300

    add task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  if (email === '' || password === '') {
+    alert('Всі поля незаповнені');
+
+    return;
+  }
+
+  console.log(formData);
+  formRef.reset();
+}
