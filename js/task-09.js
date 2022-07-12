cfd3d188f46730a0c963b458ca8878091c0d18c6
Vasyl Kondruk <vkondruk16@gmail.com>
2022-07-12 18:10:29 +0300

    add task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const buttonRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const colorRef = document.querySelector('.color');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+function changeColor(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorRef.textContent = getRandomHexColor();
+}
+
+buttonRef.addEventListener('click', changeColor);
